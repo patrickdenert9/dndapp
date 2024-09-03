2b5d4756c0a6dbd10ecb5db82492700f2c43e399
Patrick Denert <pdenert@gmail.com>
2024-09-03 11:30:14 -0400

    create new spring project

---
+++ dnd/src/main/java/com/pdenert/dnd/DndApplication.java
@@ -0,0 +1,13 @@
+package com.pdenert.dnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndApplication.class, args);
+	}
+
+}
